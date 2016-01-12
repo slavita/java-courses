a274f37c26db9312cc07be31af68604e9056c1b1
slavita <slavita2014@yandex.ru>
2016-01-12 16:00:39 +0200

    Create Calculate

--- Calculate.java
+++ Calculate.java
@@ -3,7 +3,13 @@ public class Calculate {
 		System.out.println("Calculate...");
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
-		int sum = first + second;
-		System.out.println("Sum " + sum);
+		int suma = first + second;
+		int subtract = first - second;
+		int multiply = first * second;
+		int divide = first / second;
+		System.out.println("Suma " + suma);
+		System.out.println("Subtract " + subtract);
+		System.out.println("Multiply " + multiply);
+		System.out.println("Divide " + divide);
 	}
 }
