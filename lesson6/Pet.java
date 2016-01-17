6bc9de858554d29a70043fa71c17c4953101e3ec
slavita <slavita2014@yandex.ru>
2016-01-17 22:56:12 +0200

    add clinic

---
+++ lesson6/Pet.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Cheater-PC on 16.01.2016.
+ */
+public interface Pet {
+
+
+    /**
+     * Возвращает имя питомца.
+     * @return name of pet
+     */
+    String getName();
+
+    /**
+     * Задаает имя питомца.
+     * @param name  name of pet
+     */
+    void setName(final String name);
+
+}
