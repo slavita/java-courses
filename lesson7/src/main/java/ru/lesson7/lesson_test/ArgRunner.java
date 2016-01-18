e0378f5d05c5e6d4ebe0b895d9e926459ce079d6
slavita <slavita2014@yandex.ru>
2016-01-18 12:42:31 +0200

    add test

---
+++ lesson7/src/main/java/ru/lesson7/lesson_test/ArgRunner.java
@@ -0,0 +1,45 @@
+package ru.lesson7.lesson_test;
+
+public class ArgRunner{
+	/**
+	 * @(int ... params) метод с переменным количеством параметров
+	 * класс который добавляет операции.
+	*/
+	/**
+	 * выполнение операции суммирования.
+	 */
+	public void suma(int ... params){
+		for(Integer param : params){
+			Calculator.result += param;
+		}
+	}
+	/**
+	 * выполнение операции вычитания.
+	*/
+	public void subtract(int ... params) {
+		Calculator.result = params[0];
+		for (int i = 1; i < params.length; i++) {
+			Calculator.result -= params[i];
+		}
+	}
+	/**
+	 * выполнение операции умножения.
+	 */
+	public void multiply(int ... params){
+		Calculator.result = 1;
+		for(Integer param : params){
+			Calculator.result *= param;
+		}
+	}
+	/**
+	 * выполнение операции деления.
+	 */
+	public void divide(int ... params){
+		Calculator.result = params[0];
+		for(int i = 1; i < params.length; i++){
+			Calculator.result /=  params[i];
+		}
+	}
+
+	
+}
---
+++ lesson7/src/main/java/ru/lesson7/lesson_test/Main.java
@@ -0,0 +1,26 @@
+package ru.lesson7.lesson_test;
+/**
+ * класс который содержит главную функцию программы
+ */
+public class Main {
+
+    /**
+     * InteractRunner класс пользовательского ввода.
+     * выполняем операции пока переменная exit будет равна "no"
+     */
+    public static void main(String[] arg){
+
+        Calculator calculator = new Calculator();
+        InteractRunner interactRunner = new InteractRunner();
+
+            while(!interactRunner.exit.equals("yes")) {
+              interactRunner.reusingResult();
+                interactRunner.setNumbersfirrst();
+                interactRunner.setNumberssecond();
+                interactRunner.setOperation();
+                calculator.calculation(interactRunner.first,interactRunner.second,interactRunner.operation);
+                System.out.println("Result : " + calculator.getResult());
+                interactRunner.exit();
+            }
+    }
+}
