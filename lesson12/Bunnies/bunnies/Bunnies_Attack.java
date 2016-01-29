03159731e4807f3f65476259631fba13a6a84618
slavita <slavita2014@yandex.ru>
2016-01-29 22:22:48 +0200

    add project

---
+++ lesson12/Bunnies/bunnies/Bunnies_Attack.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Cheater-PC on 29.01.2016.
+ */
+public class Bunnies_Attack {
+    public static void main(String[] args) throws InterruptedException {
+
+
+      new Thread(new BunniesPrinter()).start();
+        }
+    }
+
+
+
+
+
+
+
